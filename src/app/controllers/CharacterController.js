c0a84e105f6f4572851b3f0125deadc0c06d6abb
Tran Nam Son <trannamson0328@gmail.com>
2024-04-21 18:17:50 +0700

    CREATE NEW CHARACTER FUNCTION

---
+++ src/app/controllers/CharacterController.js
@@ -0,0 +1,30 @@
+const Course = require('../models/Course')
+const {multipleMongooseToObject} = require('../../util/mongoose')
+class CharacterController {
+// HOME
+// [GET] /
+// CONTROLLER MVC
+async index(req, res, next) {
+    // MODEL MVC
+    try {
+        // TAKE DATA MVC
+        let courses = await Course.find({});
+        // return VIEWS MVC
+        res.render('home', {courses: multipleMongooseToObject(courses)})
+
+    } catch (error) {
+        next(error)
+        // res.status(400).json({error: 'ERROR!!!'});
+    }
+    // res.render('home');
+}
+
+// SEARCH
+// [GET] /search
+search(req, res) {
+    res.render('search');
+}
+}
+
+// xuất thành file public cho cả project
+module.exports = new CharacterController();
