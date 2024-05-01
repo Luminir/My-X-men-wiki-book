a33cb5559f7817cf1a538dfe64c169fbccb974ec
Tran Nam Son <trannamson0328@gmail.com>
2024-05-01 11:57:25 +0700

    done 2

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/Xmen'); // auto create a collection called "courses", u just need to create a database
+        await mongoose.connect('mongodb://localhost:27017/Xmen'); // auto create a collection called "courses", u just need to create a database ( mine DB here is "Xme")
         console.log("Connect succesfully")
     }catch(error){
         console.log("Connect Failed")
